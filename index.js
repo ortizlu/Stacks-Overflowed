8f4c9e239e21008c50bbde47d91d45bef69339d1
Luis Ortiz <ortizlu@icloud.com>
2018-09-24 10:32:37 -0400

    test

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const bodyParser = require ('body-parser')
-const app = express() 
+const bodyParser = require('body-parser')
+const app = express()
 
+;('test')
 
+app.use(require('./routes/index.js'))
 
-app.use(require("./routes/index.js"))
-
-app.listen(3000, () => console.log("server is running"));
-
+app.listen(3000, () => console.log('server is running'))
